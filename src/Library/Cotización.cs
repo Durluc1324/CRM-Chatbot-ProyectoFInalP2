2bd23ca4e34a482603ec97a68d69ee524eb99855
Durluc1324 <lucianorodriguezpaven@gmail.com>
2025-10-18 21:20:36 -0300

    Adición de contenido dentro de clase Venta.cs y Cotización.cs

--- src/Library/Cotización.cs
+++ src/Library/Cotización.cs
@@ -2,5 +2,23 @@ namespace Library;
 
 public class Cotización
 {
-    
+    public string Motivo { get; set; }
+    public double Precio { get; set; }
+    public DateTime Fecha { get; set; }
+
+    public Cotización(string unMotivo, double unPrecio, DateTime? unaFecha=null)
+    {
+        Motivo = unMotivo;
+        Precio = unPrecio;
+        Fecha = unaFecha ?? DateTime.Now;
+    }
+
+    public void MostrarCotización()
+    {
+        Console.WriteLine($"Motivo: {Motivo}\n" +
+                          $"Precio ofrecido: {Precio}\n" +
+                          $"Fecha de cotización: {Fecha:dd/MM/yyyy HH:mm}");
+    }
+
+
 }
--- src/Library/Venta.cs
+++ src/Library/Venta.cs
@@ -2,5 +2,30 @@ namespace Library;
 
 public class Venta
 {
-    
+    public string Objeto { get; set; }
+    public double Precio { get; set; }
+    public DateTime Fecha { get; private set; }
+    public string Motivo { get; set; }
+
+    public Venta(double unPrecio, string unMotivo, DateTime? unaFecha = null)
+    {
+        Precio = unPrecio;
+        Fecha = unaFecha ?? DateTime.Now;
+        Motivo = unMotivo;
+    }
+
+    /// <summary>
+    /// Crear las ventas de la siguiente manera:
+    /// Venta v1 = new Venta(500, "Venta de laptop"); // usa fecha actual
+    /// Venta v2 = new Venta(600, "Venta antigua", new DateTime(2025, 6, 4)); // fecha específica
+    /// </summary>
+
+    public void MostrarVenta()
+    {
+        Console.WriteLine($"Producto: {Objeto}\n" +
+                          $"Precio: {Precio}\n" +
+                          $"Fecha de venta: {Fecha:dd/MM/yyyy HH:mm}\n" +
+                          $"Motivo de venta: {Motivo}\n");
+    }
+
 }
